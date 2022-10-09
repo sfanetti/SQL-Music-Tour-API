beab6854a2014a2700afafce66657f5f5f8e5237
Scott Fanetti <scott.fanetti@gmail.com>
2022-10-09 14:15:38 -0400

    updated to add controllers

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const bandsController = require('./controllers/bands_controller');
+const eventsController = require('./controllers/events_controller');
+const stagesController = require('./controllers/stages_controller');
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/bands', bandsController);
+app.use('/events', eventsController);
+app.use('/stages', stagesController);
 
 // ROOT
 app.get('/', (req, res) => {
